8ade6a4724c06ad5a90702631e4cfec019a46d6e
Sunnyshineshow <sun.napon11@gmail.com>
2023-06-22 09:27:58 +0200

    Multiprocessing update

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 # Before execution, do --> source /scratch-local/testenv/env/bin/activate
 from data_process import selectData, getMetaParam
+from delay import delay
+
+import sys
 
 import base64
 from io import BytesIO
@@ -13,10 +16,21 @@ from matplotlib.figure import Figure
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 
 import requests
+from multiprocessing import Process, Value
+import os, signal
+
+def test_delay():
+    print("I'm running!")
+    delay()
+    print("I finished running!")
+    process_status.value = 0
+    os.kill(os.getpid(),signal.SIGTERM)
+
+process_status = Value('i')
+process_status.value = 0
 
 APP = flask.Flask(__name__)
 CORS(APP)
-
 
 @APP.route("/getdomain", methods=["POST"])
 def getdomain():
@@ -46,6 +60,17 @@ def getalldata():
         retVal = selectData({})
         return flask.jsonify(response=retVal)
 
+@APP.route("/test_thread")
+def test_thread():
+    p = Process(target=test_delay)
+    p.daemon = True
+    p.start()
+    process_status.value = 1
+    return "Process Started"
+
+@APP.route("/track_status")
+def track_status():
+    return str(process_status.value)
 
 @APP.route("/ping")
 def ping():
